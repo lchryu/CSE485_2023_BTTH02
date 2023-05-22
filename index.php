a5150fca42237c60209a88c434265ca28dd0ce0b
LiliNguyen1612 <nnanhduong1612@gmail.com>
2023-05-22 23:55:15 +0700

    frontend index.php

--- index.php
+++ index.php
@@ -1,15 +1,92 @@
-<?php
-// index.php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TLU ATTENDANCE</title>
+        <link rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <style>
+    .custom-header {
+      background-color: #f8f9fa;
+    }
+    .custom-header .navbar-brand {
+      color: #dc3545;
+      font-weight: bold;
+    }
+    .custom-header .nav-link {
+      color: #333;
+    }
+    .custom-footer {
+      background-color: #f8f9fa;
+      padding: 20px 0;
+      text-align: center;
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+    }
+    .custom-grid {
+      margin-top: 20px;
+    }
+    .custom-grid .card {
+      border: 1px solid black;
+      padding: 20px;
+      border-radius: 5px;
+      margin-bottom: 20px;
+    }
+  </style>
+    </head>
+    <body>
+        <header class="navbar navbar-expand-lg navbar-light custom-header">
+            <a class="navbar-brand" href="#">TLU ATTENDANCE</a>
+            <div class="navbar-nav mr-auto">
+                <a class="nav-link" href="index.html">Home</a>
+            </div>
+            <div class="navbar-nav ml-auto">
+                <a class="nav-link" href="login.html">Login</a>
+            </div>
+        </header>
 
-// Import Connection class
-require_once 'config/Connection.php';
+        <div class="container">
+            <div class="row custom-grid">
+                <div class="col-lg-4">
+                    <div class="card" style="width: 100%;">
+                        <a href="login.html"><img src="./image/subject.webp" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                            <a href="login.html"><h5 class="card-title">Thuật toán ứng dụng</h5></a>
+                            <p class="card-text">Some quick example text to
+                                build on the card title and make up the bulk of
+                                the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card" style="width: 100%;">
+                        <a href="login.html"><img src="./image/subject.webp" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                            <a href="login.html"><h5 class="card-title">Cơ sở dữ liệu</h5></a>
+                            <p class="card-text">Some quick example text to
+                                build on the card title and make up the bulk of
+                                the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card" style="width: 100%;">
+                        <a href="login.html"><img src="./image/subject.webp" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                            <a href="login.html"><h5 class="card-title">Công nghệ WEB</h5></a>
+                            <p class="card-text">Some quick example text to
+                                build on the card title and make up the bulk of
+                                the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-// Create a new instance of the Connection class
-$connection = new Connection();
-
-// Establish a database connection
-$pdo = $connection->connect();
-
-// Include the login view
-include 'views/login.php';
-?>
+        </div>
+        
+        <footer class="custom-footer">
+            <p>&copy; 2023 TLU ATTENDANCE. All rights reserved.</p>
+        </footer>
+    </body>
+</html>
